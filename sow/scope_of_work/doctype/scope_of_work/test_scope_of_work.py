be642ce9ba0d524554cb7d49b9bb0a5b90df5f22
Nirmal <nirmal.r@brilworks.com>
2024-07-11 11:43:26 +0530

    sow app

---
+++ sow/scope_of_work/doctype/scope_of_work/test_scope_of_work.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, nirmal.e@brilworks.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestScopeofwork(FrappeTestCase):
+	pass
